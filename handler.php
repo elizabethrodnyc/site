1de162417d01b48ca286f0d48eb49c3c485e3f71
Elizabeth Rodricks <elizabethrodricks@Elizabeths-iMac.local>
2025-04-14 16:17:35 -0400

    site

--- handler.php
+++ handler.php
@@ -21,6 +21,6 @@ $validator->field('message')->maxLength(6000);
 
 
 
-$pp->sendEmailTo(['rodricksnyc@gmail.com']); // ← Your email here
+$pp->sendEmailTo(['']); // ← Your email here
 
 echo $pp->process($_POST);
